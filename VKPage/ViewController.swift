de57f4ad368c6c7651caa16f4d6e24108875f90a
Chingiz <dzhabaev@outlook.com>
2023-06-27 11:58:04 +0400

    Initial Commit

---
+++ VKPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKPage
+//
+//  Created by Чингиз Джабаев on 27.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
